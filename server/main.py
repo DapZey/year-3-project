8c92d485bbff20c82cdcdbd1bff4542df7d9a102
DapZey <72968513+DapZey@users.noreply.github.com>
2024-12-16 15:25:43 +0200

    Update main.py

--- server/main.py
+++ server/main.py
@@ -1,8 +1,15 @@
 import network
-
+from multiprocessing import freeze_support
+from transformers import CLIPProcessor, CLIPModel
 if __name__ == '__main__':
     try:
-        network.main()
+        print("starting server...")
+        freeze_support()
+        model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+        processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+        print("start finished")
+        network.main(model, processor)
+
     except OSError as e:
         print("Error loading model. Make sure you have an internet connection and try running:")
         print("pip install --upgrade transformers")
